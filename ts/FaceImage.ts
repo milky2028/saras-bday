a95fa02e2c2b815526677509a4f96b8305962b66
Tyler Gross <tylergross28@gmail.com>
2019-04-06 20:25:49 -0500

    base for face image and placement

--- ts/FaceImage.ts
+++ ts/FaceImage.ts
@@ -1,7 +1,42 @@
-export default class FaceImage {
+export interface FaceImageOptions {
   src: string;
+}
 
-  constructor(src: string) {
-    this.src = src;
+export default class FaceImage {
+  private img!: HTMLImageElement;
+
+  constructor(ctx: CanvasRenderingContext2D, options: FaceImageOptions) {
+    this.img = new Image();
+    this.img.src = options.src;
+
+    this.img.addEventListener('load', () => {
+      ctx.drawImage(this.img, 0, 0, this.imageSquare, this.imageSquare);
+    });
+  }
+
+  get startingY() {
+    return 0 - this.imageSquare;
+  }
+
+  get dpi() {
+    return window.devicePixelRatio;
+  }
+
+  get imageSquare() {
+    return (this.w <= 375)
+    ? this.dpi * 330
+    : this.w / 2;
+  }
+
+  get w() {
+    return (window.screen.width > 375)
+    ? window.innerWidth
+    : window.screen.width;
+  }
+
+  get h() {
+    return (window.screen.height > 375)
+    ? window.innerHeight
+    : window.screen.height;
   }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -1,15 +1,27 @@
 import SetCanvasSize from './SetCanvasSize.js';
-import FaceImage from './FaceImage.js';
+import FaceImage, { FaceImageOptions } from './FaceImage.js';
 
 const canvas = document.querySelector('canvas');
 const h = window.innerHeight;
 const w = window.innerWidth;
 const ctx = canvas!.getContext('2d');
 
+const images = [
+  'img/both-of-us.jpg',
+  'img/sara.jpg'
+];
+
+function init() {
+  const randomImageNumber = Math.floor(Math.random() * images.length);
+  const options: FaceImageOptions = {
+    src: images[randomImageNumber]
+  };
+
+  const f = new FaceImage(ctx!, options);
+}
+
+init();
 SetCanvasSize(canvas!, w, h);
 window.addEventListener('resize', () => {
   SetCanvasSize(canvas!, w, h);
 });
-
-ctx!.fillStyle = 'blueviolet';
-ctx!.fillRect(0, 0, w, h);
